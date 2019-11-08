563cde9e98f469ace1ccd4f2c1b02811dd8d2ba8
Chen Chen <chen.chen.ece@gmail.com>
2019-11-08 16:29:21 -0500

    remove stale copy

--- include/net/arch/perror.h
+++
@@ -1,7 +0,0 @@
-#ifndef LEGO_INC_PERROR_H
-#define LEGO_INC_PERROR_H
-
-void perror(const char *s);
-const char *e2s(int e);
-
-#endif /* !LEGO_INC_PERROR_H */
--- net/lwip/lego/jif/jif.h
+++
@@ -1,9 +0,0 @@
-#include <net/lwip/netif.h>
-
-struct jif_pkt {
-        int jp_len;
-	char *jp_data;
-};
-
-void	jif_input(struct netif *netif, void *va);
-err_t	jif_init(struct netif *netif);
