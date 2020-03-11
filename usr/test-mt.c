167ec209f025c2682cfc78a965d675b57577b796
Shreeman Gokul Hariharan <sghariha@gmail.com>
2020-03-10 17:48:51 -0700

    Added mt user program

--- usr/test-mt.c
+++ usr/test-mt.c
@@ -6,7 +6,7 @@ void printMsg(void *num) {
     int i = 0;
     int *num_ptr = (int *)num;
     for (i = 0; i < 10; i++) {
-        printf("Thread number is %d", *num);
+        printf("Thread number is %d", *num_ptr);
     }
 }
 
@@ -14,8 +14,8 @@ void main() {
     pthread_t tid1, tid2; 
     int a = 1;
     int b = 2;
-    pthread_create(&tid1, NULL, thread1, &a);
-    pthread_create(&tid1, NULL, thread1, &b);
+    pthread_create(&tid1, NULL, printMsg, &a);
+    pthread_create(&tid1, NULL, printMsg, &b);
     pthread_join(tid1, NULL); 
     pthread_join(tid2, NULL); 
 }
