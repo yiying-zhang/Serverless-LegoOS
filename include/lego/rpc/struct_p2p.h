fd4469b4fb12c20232fea56fec8670003516c6b8
lih666 <jaslihao@gmail.com>
2020-03-19 22:54:43 -0700

    updates

--- include/lego/rpc/struct_p2p.h
+++ include/lego/rpc/struct_p2p.h
@@ -14,7 +14,7 @@
 #include <lego/types.h>
 #include <lego/kernel.h>
 
-#define MAX_P2P_MSG_LEN 2048
+#define MAX_P2P_MSG_LEN 4096
 
 struct p2p_msg_hdr {
 	unsigned int opcode;		/* OPCODE should be useless in our case */
